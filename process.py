9437998d9028784c3ba722cbbcb5fe7957dda551
Krish-saama <gopalakrishnan.palanisamy@saama.com>
2022-05-09 00:05:37 +0530

    issue fixed

--- process.py
+++ process.py
@@ -23,11 +23,13 @@ class check:
         data = json.loads(lsres.data)
         # print(data['result'])
         # print("installed_packages", installed_packages)
+        
         for fetch in data['result']:
-            url = "https://api.github.com/repos/"+ fetch['accountname'] +"/"+ fetch['repositoriename'] +"/contents/requirementsjson.txt?ref="+ fetch['branchname'] +""
+            lsreponame = fetch['repositoriename']
+            url = "https://api.github.com/repos/"+ fetch['accountname'] +"/"+ lsreponame +"/contents/requirementsjson.txt?ref="+ fetch['branchname'] +""
             payload={}
             headers = {
-              'Authorization': 'token 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'
+              'Authorization': 'token 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'
             }            
             strresponse = requests.request("GET", url, headers=headers, data=payload)
             print(strresponse.text)
@@ -37,19 +39,20 @@ class check:
             print(lscon)
 
             lsreqjson = json.loads(lscon)
-            for k in lscon:
-                print(k, lscon[k])
+            for k in lsreqjson:
+                print(k, lsreqjson[k])
                 # for line in lscon.splitlines():
                 #   print(line)
                 #   lineResult = libLAPFF.parseLine(line)
                 # lspac = fetch['packagename']
                 lspac = k
-                lsinstalledver = installed_packages[lspac.title()]
+                lsinstalledver = installed_packages[lspac]
                 lsvercheck = bool(lsinstalledver and not lsinstalledver.isspace())
                 url = f'https://pypi.python.org/pypi/{lspac}/json'
                 releases = json.loads(request.urlopen(url).read())['info']
                 lsversion = str(releases["version"])
                 lssummary = releases["summary"]
+                lsrelease = releases["release_url"]
                 lsinstpack = lspac + " " +lsversion
                 lsintlatestver = int(lsversion.replace(".", ""))
                 if(lsvercheck):
@@ -64,21 +67,20 @@ class check:
                     #     [sys.executable, '-m', 'pip', lspac])
                     # summary
                 #  lsres = dbaccess.dbaccessdetails.updateframwork(lspac,lsversion)
-                    check.mailsend(lsinstpack,lssummary)
+                    check.mailsend(lsreponame,lspac,lsinstalledver,lsversion,lsrelease,lssummary)
                     print("Successfully installed")
                 else:
                     print(lsinstpack, "Already installed latest")
         return lsres
 
-    def mailsend(pspackage, psdescription):
+    def mailsend(reponame,pspackage,currver, latestver, releaseurl, psdescription):
         #   label = subprocess.check_output(["git", "describe"]).strip()
         #   print("git user details: ", label)
         # create message object instance
         msg = MIMEMultipart()
-        message = "<h1>Installed the package latest version: " + \
-            pspackage + "</h1><b><p>Summary: " + psdescription + "</p>"
+        message = "<h1>Installed the package version Details: </h1><b><p>Repo name: "+ reponame +"</p><b><p>Package name: "+ pspackage +"</p><b><p>Current used version: "+ currver +"</p><b><p>Available latest version: "+ latestver +"</p><b><p>Relase URL: "+ releaseurl +"</p><b>"
         # setup the parameters of the message
-        password = "@gk8072721917"
+        password = "@gk8072721917@"
         msg['From'] = "pgopalakrishnan1996@gmail.com"
         msg['To'] = "Gopalakrishnan.palanisamy@saama.com"
         msg['Cc'] = "Gopalakrishnan.palanisamy@saama.com"
